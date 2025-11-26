cf54520da969f96e8ddbcddd4009adba4649e3b5
isafeitosacode <isadoracastrofeitosa@gmail.com>
2025-11-26 10:20:28 -0300

    log

--- apps/server/server.js
+++ apps/server/server.js
@@ -8,6 +8,10 @@ const db = require('./db/knex.js');
 const routes = require('./routes/routes.js');
       
 const app = express();
+app.use((req, res, next) => {
+  console.log(`📢 RECEBI: ${req.method} ${req.url}`);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
